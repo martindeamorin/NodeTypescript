a1c924cfa5113e98b415c05c078d0191f31fdc86
Martín de Amorín <martindeamorin@gmail.com>
2021-07-28 21:43:15 -0300

    prueba vercel

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -2,6 +2,6 @@ import {Router, Request, Response} from "express";
 const router: Router = Router();
 import {homeController} from "../controllers/homeController"
 
-router.get("/", homeController.home)
+router.get("/hola", homeController.home)
 
 export default router;
